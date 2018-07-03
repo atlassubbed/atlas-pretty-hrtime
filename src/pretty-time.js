829b6a7ba7c6bc8b26aaace8d789f306cc248228
atlassubbed <atlassubbed@gmail.com>
2018-07-02 22:05:56 -0400

    makes naming more intuitive

--- src/pretty-time.js
+++ src/pretty-time.js
@@ -1,16 +1,18 @@
 const { isNum, getAscendingUnits } = require("./util")
 
-const units = getAscendingUnits({us: 1e3, ms: 1e6, sec: 1e9, min: 6e10, hr: 36e11})
+const possibleScales = {us: 1e3, ms: 1e6, sec: 1e9, min: 6e10, hr: 36e11}
+
+const units = getAscendingUnits(possibleScales)
 
 module.exports = (time, precision=3) => {
   if (!isNum(time)) 
     throw new Error("requires a number in nanoseconds");
-  // return first reasonable match
+  // returns first largest match
   for (let i = units.length; i--;){
-    const { unit, factor } = units[i], nextUnit = units[i-1];
-    const nextFactor = nextUnit ? nextUnit.factor : 1;
-    const smallerUnitTime = Number((time/nextFactor).toFixed(precision))
-    if (Math.abs(smallerUnitTime) >= factor/nextFactor){
+    const { unit, factor } = units[i], smallerUnit = units[i-1];
+    const smallerFactor = smallerUnit ? smallerUnit.factor : 1;
+    const roundedTimeInSmallerUnit = Number((time/smallerFactor).toFixed(precision))
+    if (Math.abs(roundedTimeInSmallerUnit) >= factor/smallerFactor){
       return (time/factor).toFixed(precision)+unit
     }
   }
